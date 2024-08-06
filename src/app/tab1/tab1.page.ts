415479589352e4e7a2beafdd281b958371d7aa75
saranyapat boonngam <saranyapa1234t@gmail.com>
2024-08-06 14:30:23 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent ,IonItem ,IonLabel ,IonButton ,IonActionSheet ,IonAlert ,IonList ,IonBadge ,IonCheckbox} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,8 +7,74 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent ,IonItem ,IonLabel ,IonButton ,IonActionSheet ,IonAlert ,IonList ,IonBadge ,IonCheckbox],
 })
 export class Tab1Page {
   constructor() {}
+  isActionSheetOpen = false;
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
+
+  public alertButtons = [
+    {
+      text: 'ยกเลิก',
+      role: 'ยกเลิก',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'ยืนยัน',
+      role: 'ยืนยัน',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
+  public alertButton1 = ['OK'];
+  public alertInputs = [
+    {
+      placeholder: 'ชื่อ-นามสกุล',
+    },
+    {
+      placeholder: 'ชื่อเล่น',
+      attributes: {
+        maxlength: 8,
+      },
+    },
+    {
+      type: 'number',
+      placeholder: 'อายุ',
+      min: 1,
+      max: 100,
+    },
+    {
+      type: 'textarea',
+      placeholder: 'เกี่ยวกับตัวเอง',
+    },
+  ];
 }
